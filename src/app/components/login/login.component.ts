25b74ff5f6a5d1819e1d4a2a10eb19a5be9ade84
Wesley <wesleykok-@hotmail.com>
2020-09-19 14:53:12 +1000

    Registration form and login form complete

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -8,12 +7,34 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-  loading = false;
-  submitted = false;
-  returnUrl: string;
+  show: boolean = false; // show hidden div
+  errorMsg: string = '';
 
-  constructor() {}
+  userRegForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    let email = this.userRegForm.controls['email'].value;
+    let password = this.userRegForm.controls['password'].value;
+    if (this.userRegForm.valid) {
+      // valid details
+      this.show = false;
+      this.errorMsg = '';
+      console.log(email);
+      console.log(password);
+    } else {
+      //invalid email or missing pw
+      this.show = true;
+      if (this.userRegForm.get('email').status == 'INVALID')
+        this.errorMsg = 'Please check your email format!';
+      else if (this.userRegForm.get('password').status == 'INVALID')
+        this.errorMsg = 'Please enter your password!';
+    }
+  }
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -10,10 +10,18 @@ export class RegisterComponent implements OnInit {
   show: boolean = false; // show hidden div
   errorMsg: string = '';
 
-  userRegForm = this.formBuilder.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required],
-  });
+  userRegForm = this.formBuilder.group(
+    {
+      fName: ['', Validators.required],
+      lName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      cPassword: ['', Validators.required],
+    },
+    {
+      validator: this.checkPassword('password', 'cPassword'),
+    }
+  );
 
   constructor(private formBuilder: FormBuilder) {}
 
@@ -31,8 +39,35 @@ export class RegisterComponent implements OnInit {
     } else {
       //invalid email or missing pw
       this.show = true;
-      if (!password) this.errorMsg = 'Please choose a password!';
-      else this.errorMsg = 'Please check your email format!';
+      if (this.userRegForm.get('fName').status == 'INVALID')
+        this.errorMsg = 'Please enter first name!';
+      else if (this.userRegForm.get('lName').status == 'INVALID')
+        this.errorMsg = 'Please enter last name!';
+      else if (this.userRegForm.get('email').status == 'INVALID')
+        this.errorMsg = 'Please check your email format!';
+      else if (this.userRegForm.get('password').status == 'INVALID')
+        this.errorMsg = 'Please enter a password longer than 6 characters!';
+      else if (this.userRegForm.get('cPassword').status == 'INVALID')
+        this.errorMsg = 'Your password does not match!';
     }
+  }
+
+  checkPassword(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+        // return if another validator has already found an error on the matchingControl
+        return;
+      }
+
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
   }
 }
